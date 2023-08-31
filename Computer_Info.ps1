e0c06bd827e0e746b0ee2063677d66b405a87be2
y2clay <96666602+y2clay@users.noreply.github.com>
2023-08-31 23:57:59 +0300

    Initial Commit

---
+++ Computer_Info.ps1
@@ -0,0 +1,12 @@
+# Created by Brandon Hatfield
+# 20230829
+# This script was created to facilitate gathering of basic computer information on an enterprise network
+
+Add-Type -AssemblyName PresentationFramework
+
+$ComputerName = (HostName)
+$SerialNumber = (Get-WmiObject -class win32_bios).SerialNumber
+$MacAddress = (Get-NetAdapter | Where-object {$_.Status -match 'Up'}).MacAddress
+$IPAddress = (Get-NetIPAddress -AddressFamily IPv4 -AddressState Preferred).IPAddress
+
+$msgBoxInput =  [System.Windows.MessageBox]::Show("Computer Name : $ComputerName `nSerial # $SerialNumber `nMac Adress : $MacAddress `nIP Address $IPAddress","Computer Info")
